96fbcb34ee0926d3590f21419e2966a5d32eccc8
Marc Tönsing <marc@marc.tv>
2019-06-08 22:10:46 +0200

    Added shortscore list styling

--- functions.php
+++ functions.php
@@ -12,4 +12,109 @@ function flower_theme_setup() {
 	add_image_size( 'yarpp', 231, 100, true ); // yarpp image
 	add_image_size( 'yarpp-retina', 462, 200, true ); // yarpp image
 }
+
+/* SHORTSCORE */
+
+function get_shortscore_list() {
+
+	$args = array(
+		'post_type'      => 'post',
+		'orderby'        => array( 'meta_value_num' => 'DESC', 'date' => 'DESC' ),
+		'meta_key'       => '_shortscore_user_rating',
+		'posts_per_page' => '300',
+		'order'          => 'DESC'
+	);
+
+	$the_query = new WP_Query( $args );
+	$html      = '';
+	$score     = '';
+
+	while ( $the_query->have_posts() ) :
+		$the_query->the_post();
+		$result =  get_post_meta( get_the_ID(), "_shortscore_result", true );
+		$result = json_decode(json_encode($result));
+		if ( isset( $result->game ) AND isset( $result->game->title ) ) {
+			$title =  $result->game->title;
+		}
+		$shortscore = get_post_meta( get_the_ID(), "_shortscore_user_rating", true );
+
+		if ( $score != $shortscore AND $shortscore > 0 ) {
+			if ( $score != '' ) {
+				$html .= "</ul> \n";
+			}
+			$html .= '<h2>SHORTSCORE ' . $shortscore . '/10</h2>';
+			$html .= '<ul>';
+		}
+
+		if ( $title != '' AND $shortscore != '' ) {
+			$html .= '<li>';
+			$html .= '[' . $shortscore . '/10] - <a href="' . get_permalink() . '">' . $title . '</a>';
+			$html .= "</li> \n";
+		}
+
+		$score = $shortscore;
+	endwhile;
+
+	return $html;
+}
+
+
+function get_shortscore_table() {
+
+	$args = array(
+		'post_type'      => 'game',
+		'orderby'        => 'meta_value_num',
+		'orderby'        => array( 'meta_value_num' => 'DESC', 'date' => 'DESC' ),
+		'meta_key'       => 'score_value',
+		'posts_per_page' => '300',
+		'order'          => 'DESC'
+	);
+
+	$the_query = new WP_Query( $args );
+	$html      = '<table class="GeneratedTable">
+  <thead>
+    <tr>
+      <th>Score</th>
+      <th>Votes</th>
+      <th>Game</th>
+      <!-- <th>Developer</th> -->
+      <th>Release Date</th>
+    </tr>
+  </thead>
+  <tbody>';
+	$score     = '';
+
+	while ( $the_query->have_posts() ) :
+		$the_query->the_post();
+		$gid         = get_the_ID();
+		$shortscore  = get_post_meta( $gid, "score_value", true );
+		$title       = get_the_title( $gid );
+		$score_count = get_post_meta( $gid, "score_count", true );
+		//$developer_list = get_the_term_list( $gid, 'developer', '', ', ' );
+		$releasedate = get_the_date( 'd. m. Y', $gid );
+
+		$html .= '<tr>';
+		$html .= '<td>';
+		$html .= $shortscore . '/10';
+		$html .= "</td> \n";
+		$html .= '<td>';
+		$html .= $score_count;
+		$html .= "</td> \n";
+		$html .= '<td>';
+		$html .= '<a href="' . get_permalink() . '">' . $title . '</a>';
+		$html .= "</td> \n";
+		//$html .= '<td>';
+		//$html .= $developer_list;
+		//$html .= "</td> \n";
+		$html .= '<td>';
+		$html .= $releasedate;
+		$html .= "</td> \n";
+
+		$html .= '</tr>';
+	endwhile;
+	$html .= '  </tbody>
+</table>';
+
+	return $html;
+}
 ?>
---
+++ page-templates/sortbyreview.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Template Name: Game Ranking
+ *
+ * Description: A custom game ranking template
+ *
+ */
+
+get_header();
+?>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<div class="entry-content">
+				<?php
+
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<?php if ( ! twentynineteen_can_show_post_thumbnail() ) : ?>
+						<header class="entry-header">
+							<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+						</header>
+						<?php endif; ?>
+
+						<div class="entry-content">
+							<?php
+							the_content();
+							echo get_shortscore_list();
+
+							wp_link_pages(
+								array(
+									'before' => '<div class="page-links">' . __( 'Pages:', 'twentynineteen' ),
+									'after'  => '</div>',
+								)
+							);
+							?>
+						</div><!-- .entry-content -->
+
+						<?php if ( get_edit_post_link() ) : ?>
+							<footer class="entry-footer">
+								<?php
+								edit_post_link(
+									sprintf(
+										wp_kses(
+											/* translators: %s: Name of current post. Only visible to screen readers */
+											__( 'Edit <span class="screen-reader-text">%s</span>', 'twentynineteen' ),
+											array(
+												'span' => array(
+													'class' => array(),
+												),
+											)
+										),
+										get_the_title()
+									),
+									'<span class="edit-link">',
+									'</span>'
+								);
+								?>
+							</footer><!-- .entry-footer -->
+						<?php endif; ?>
+					</article><!-- #post-<?php the_ID(); ?> -->
+					 <?php
+
+
+				endwhile; // End of the loop.
+				?>
+
+			</div>
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
+?>
