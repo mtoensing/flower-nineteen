33084f9e02e8990cf8ffc2162329ea0e2856fe90
Marc Tönsing <marc@marc.tv>
2019-06-09 18:29:41 +0200

    Fixed single.php problem

--- single.php
+++ single.php
@@ -1,13 +1,8 @@
 <?php
 /**
- * The main template file
+ * The template for displaying all single posts
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package WordPress
  * @subpackage Twenty_Nineteen
@@ -20,28 +15,46 @@ get_header();
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) {
+			<?php
 
-			// Load posts loop.
-			while ( have_posts() ) {
+			/* Start the Loop */
+			while ( have_posts() ) :
 				the_post();
-				get_template_part( 'template-parts/content/content' );
-			}
 
-			// Previous/next page navigation.
-			twentynineteen_the_posts_navigation();
+				get_template_part( 'template-parts/content/content', 'single' );
 
-		} else {
+				if ( is_singular( 'attachment' ) ) {
+					// Parent post navigation.
+					the_post_navigation(
+						array(
+							/* translators: %s: parent post link */
+							'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentynineteen' ), '%title' ),
+						)
+					);
+				} elseif ( is_singular( 'post' ) ) {
+					// Previous/next post navigation.
+					the_post_navigation(
+						array(
+							'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'twentynineteen' ) . '</span> ' .
+								'<span class="screen-reader-text">' . __( 'Next post:', 'twentynineteen' ) . '</span> <br/>' .
+								'<span class="post-title">%title</span>',
+							'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'twentynineteen' ) . '</span> ' .
+								'<span class="screen-reader-text">' . __( 'Previous post:', 'twentynineteen' ) . '</span> <br/>' .
+								'<span class="post-title">%title</span>',
+						)
+					);
+				}
 
-			// If no content, include the "No posts found" template.
-			get_template_part( 'template-parts/content/content', 'none' );
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
 
-		}
-		?>
+			endwhile; // End of the loop.
+			?>
 
-		</main><!-- .site-main -->
-	</section><!-- .content-area -->
+		</main><!-- #main -->
+	</section><!-- #primary -->
 
 <?php
 get_footer();
