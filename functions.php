7c2a949a9b66889ce7f501f72137b634eb3e45e2
Marc Tönsing <marc@marc.tv>
2019-06-13 13:04:05 +0200

    Removed some legacy image sizes. Removed stuff from header

--- functions.php
+++ functions.php
@@ -1,16 +1,16 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'flower_enqueue_styles' );
-
-function flower_enqueue_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-}
 
 add_action( 'after_setup_theme', 'flower_theme_setup' );
 
 function flower_theme_setup() {
-	add_image_size( 'article-retina', 1480 ); // 1480 pixels wide (and unlimited height)
-	add_image_size( 'yarpp', 231, 100, true ); // yarpp image
-	add_image_size( 'yarpp-retina', 462, 200, true ); // yarpp image
+	add_image_size( 'yarpp', 460, 200, true ); // yarpp image
+	add_image_size( 'yarpp-retina', 920, 400, true ); // yarpp image
+}
+
+add_action( 'wp_enqueue_scripts', 'flower_enqueue_styles' );
+
+function flower_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
 /* SHORTSCORE */
