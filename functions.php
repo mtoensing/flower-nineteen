d6f9141332947c0b1e9282404745d34dd26c0e3f
Marc Toensing <marc.toensing@gmail.com>
2021-03-11 18:41:06 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,5 @@
 <?php
 
-/** * Remove jQuery From WordPress */
-function my_init() {
-    if (!is_admin() AND !is_admin_bar_showing()) {
-        wp_deregister_script('jquery');
-        wp_register_script('jquery', false);
-    }
-}
-
-add_action('init', 'my_init');
-
 add_filter( 'rfi_rss_image_size', 'thumbnail_size_for_rss', 10, 1 );
 
 function thumbnail_size_for_rss( $default ) {
